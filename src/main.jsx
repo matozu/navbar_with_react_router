27c98f99f1f681f19e5b714afb50cfabf88824d7
matozu <mato.zunac@gmail.com>
2024-04-26 12:27:07 +0200

    BrowserRouter basename=/navbar_with_react_router

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/navbar_with_react_router">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
